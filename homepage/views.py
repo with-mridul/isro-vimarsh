401cf41406513b425005395fb68facc3abfd220c
with-mridul <mridul.shrotriya@gmail.com>
2025-12-23 17:33:08 +0530

    added a hero section on homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from missions.models import Mission
 from news.models import NewsArticle
+from rockets.models import Rocket
 from .models import IsroAchievement
 
 def home(request):
     featured_missions = Mission.objects.filter(featured_on_homepage=True).order_by('homepage_order')[:3]
     latest_news = NewsArticle.objects.filter(is_published=True)[:3]
     
+    # Stats for quick glance section
+    total_missions = Mission.objects.count()
+    total_rockets = Rocket.objects.count()
+    total_achievements = IsroAchievement.objects.count()
+    
     context = {
         'featured_missions': featured_missions,
         'latest_news': latest_news,
+        'total_missions': total_missions,
+        'total_rockets': total_rockets,
+        'total_achievements': total_achievements,
     }
     return render(request, 'homepage/home.html', context)
 
